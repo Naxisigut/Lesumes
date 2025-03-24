25a7d7d39b3f19187754a3f0358cf2a5da8b4154
Naxisigut <834525217@qq.com>
2025-03-24 13:47:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="bg-blue-500 text-white p-2 rounded-md">Click me</button>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+</style>
